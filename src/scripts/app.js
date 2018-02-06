04103370fa1c05c898c328e081bdf865bb04f12b
Phil Lucks <philip.lucks@snapengage.com>
2018-02-05 22:39:38 -0700

    ready

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,56 +1,58 @@
-angular.module('app_litsco', [
-  'ui.router',
-  'ngMap',
-  'ui.router.title'
-])
-  .directive('myYoutube', function ($sce) {
+angular
+  .module("app_litsco", ["ui.router", "ngMap", "ui.router.title"])
+  .directive("myYoutube", function($sce) {
     return {
-      restrict: 'EA',
-      scope: { code: '=' },
+      restrict: "EA",
+      scope: { code: "=" },
       replace: true,
-      template: '<div style="height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{ url }}" frameborder="0" allowfullscreen></iframe></div>',
-      link: function (scope) {
-        scope.$watch('code', function (newVal) {
+      template:
+        '<div style="height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{ url }}" frameborder="0" allowfullscreen></iframe></div>',
+      link: function(scope) {
+        scope.$watch("code", function(newVal) {
           if (newVal) {
-            scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + newVal);
+            scope.url = $sce.trustAsResourceUrl(
+              "http://www.youtube.com/embed/" + newVal
+            );
           }
         });
       }
     };
   })
-  .directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
+  .directive("fileModel", [
+    "$parse",
+    function($parse) {
+      return {
+        restrict: "A",
         link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
+          var model = $parse(attrs.fileModel);
+          var modelSetter = model.assign;
 
-            element.bind('change', function(){
-              if (!element[0].files[0]) {
-                return;
-              } else if (scope.validateFileType(element[0].files[0])) {
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-              } else {
-                return false;
-              }
-            });
+          element.bind("change", function() {
+            if (!element[0].files[0]) {
+              return;
+            } else if (scope.validateFileType(element[0].files[0])) {
+              scope.$apply(function() {
+                modelSetter(scope, element[0].files[0]);
+              });
+            } else {
+              return false;
+            }
+          });
         }
-    };
-}])
-  .run(function($rootScope, $state, $document, $stateParams){
+      };
+    }
+  ])
+  .run(function($rootScope, $state, $document, $stateParams) {
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
-    $rootScope.$on('$stateChangeSuccess', function() {
+    $rootScope.$on("$stateChangeSuccess", function() {
       $document[0].body.scrollTop = $document[0].documentElement.scrollTop = 0;
-    }); 
-  }) 
-  .config(function (NgMapProvider) {
+    });
+  })
+  .config(function(NgMapProvider) {
     NgMapProvider.setDefaultOptions({
       marker: {
         optimized: false
       }
     });
   });
-  
--- src/scripts/components/controller_index.js
+++ src/scripts/components/controller_index.js
@@ -1,9 +1,24 @@
-angular.module('app_litsco')
-	.controller('controller_index', ['$scope', 'factory_meta', function ($scope, factory_meta) {
-		$scope.metaTag = factory_meta;	
-		
-		$scope.showNews = false;
-		$scope.toggleNews = function() {
-			$scope.showNews = !$scope.showNews;
-		};
-	}]);
+angular.module("app_litsco").controller("controller_index", [
+  "$scope",
+  "factory_meta",
+  "$timeout",
+  function($scope, factory_meta, $timeout) {
+    $scope.metaTag = factory_meta;
+
+    $scope.showNews = false;
+
+    $scope.openNews = function() {
+      if (!$scope.showNews) {
+        $scope.showNews = true;
+      }
+    };
+
+    $scope.closeNews = function() {
+      $scope.showNews = false;
+    };
+
+    $timeout(function() {
+      $scope.openNews();
+    }, 2000);
+  }
+]);
