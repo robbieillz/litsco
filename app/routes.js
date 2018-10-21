a1c6c111a388d493b69cecbb03d5b7a0717968ed
Phil Lucks <philip.lucks@snapengage.com>
2018-10-20 21:45:44 -0600

    fix form

--- app/routes.js
+++ app/routes.js
@@ -1,99 +1,146 @@
 // HTTP ROUTING
-var apiRouter = require('express').Router();
-var express = require('express');
-var multer = require('multer');
-var fs = require('fs');
-var safeKey = JSON.parse(fs.readFileSync('./safekey.json', 'utf-8'));
-var bodyParser = require('body-parser');
-var nodemailer = require('nodemailer');
+var apiRouter = require("express").Router();
+var express = require("express");
+var multer = require("multer");
+var fs = require("fs");
+var safeKey = JSON.parse(fs.readFileSync("./safekey.json", "utf-8"));
+var bodyParser = require("body-parser");
+var nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport({
-    pool: true,
-    host: safeKey.emailHost,
-    port: safeKey.emailPort,
-    secure: true,
-    auth: {
-        user: safeKey.emailUser,
-        pass: safeKey.emailPass
-    }
+  pool: true,
+  host: safeKey.emailHost,
+  port: safeKey.emailPort,
+  secure: true,
+  auth: {
+    user: safeKey.emailUser,
+    pass: safeKey.emailPass
+  }
 });
 apiRouter.use(bodyParser.urlencoded({ extended: true }));
 apiRouter.use(bodyParser.json());
 
 // Page Routes
-apiRouter.get('/', function (req, res, next) {
-    res.sendFile('index.html', { root: './src' });
+apiRouter.get("/", function(req, res, next) {
+  res.sendFile("index.html", { root: "./src" });
 });
 
-apiRouter.post('/api/contact', function (req, res) {
-    var data = req.body;
-    var mailOptions = {
-        from: data.email,
-        to: 'info@litsco.com',
-        subject: '[LITSCO CONTACT FORM] Email sent by ' + data.name,
-        text: data.message
+apiRouter.post("/api/contact", function(req, res) {
+  var formData = req.body;
+  var mailOptions = {
+    from: formData.email,
+    to: "info@litsco.com",
+    subject: "[LITSCO CONTACT FORM]",
+    html:
+      "You have received an message from your contact form:" +
+      "<ul>" +
+      "<li><b>E-mail: </b><a href='mailto:'" +
+      formData.email +
+      "?Subject=LITSCO%20Employment%20Inquiry%20Response' target='_top'>" +
+      formData.email +
+      "</a></li>" +
+      "<li><b>Name: </b>" +
+      formData.name +
+      "</li>" +
+      "<li><b>Phone: </b>" +
+      (formData.phone ? formData.phone : "not provided") +
+      "</li>" +
+      "<li><b>Company: </b>" +
+      (formData.company ? formData.company : "not provided") +
+      "</li>" +
+      "<li><b>Message: </b>" +
+      formData.message +
+      "</li>" +
+      "</ul>" +
+      "You can respond directly to this email, or create a new message clicking on the email address listed."
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    var output = {
+      flag: 1
     };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            res.json(
-                {
-                    info: 'Email not sent'
-                }, {
-                    error: error
-                });
-        } else {
-            console.log('Message sent: ' + info.response);
-            console.log('Data sent:' + data);
-            res.json(
-                {
-                    success: 'Email has been sent.'
-                }, {
-                    response: info.response
-                }, {
-                    data: data
-                });
-        }
-    });
+    if (error) {
+      console.log("** Error: ", error);
+      output.flag = 0;
+      output.error = error;
+    } else {
+      console.log("** Message sent: " + info.response);
+      console.log("** Data sent:" + data);
+      output.response = info.response;
+      output.success = true;
+      console.log("sender output: ", output);
+    }
+    res.send(output);
+  });
+  transporter.close();
 });
 
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 
-apiRouter.post('/api/career_submit', upload.single('attachment'), function (req, res) {
-    var formData = req.body;
-    var file = req.file;
+apiRouter.post("/api/career_submit", upload.single("attachment"), function(
+  req,
+  res
+) {
+  var formData = req.body;
+  var file = req.file;
 
-    var mailOptions = {
-        from: formData.email,
-        to: 'info@litsco.com',
-        subject: '[EMPLOYMENT INQUIRY FORM] | ' + formData.position,
-        html: '<p>You have receieved and email from ' + formData.first + ' ' + formData.last + ' in reference to the open position on your Careers page, for the <b>' + formData.position + '</b> position.  <br>See attachment for resume/cover letter.</p>' +
-        'Applicant Details:' +
-        '<ul><li><b>Name: </b> ' + formData.first + ' ' + formData.last + '</li><li><b>Application Position: </b>' + formData.position + '</li><li><b>E-mail: </b><a href="mailto:' + formData.email + '?Subject=LITSCO%20Employment%20Inquiry%20Response" target="_top">' + formData.email + '</a><li></li><b>Phone: </b>' + formData.phone + '<li></li><b>Address: </b>' + formData.address + '</li><li><b>They are interested in this position because: </b>' + formData.message + '</li></ul>' +
-        'You can respond directly to this email, or create a new message clicking on the email address listed.',
-        attachments: [{
-            filename: file.originalname,
-            content: file.buffer
-        }]
+  var mailOptions = {
+    from: formData.email,
+    to: "info@litsco.com",
+    subject: "[EMPLOYMENT INQUIRY FORM] | " + formData.position,
+    html:
+      "<p>You have received an email from " +
+      formData.first +
+      " " +
+      formData.last +
+      " in reference to the open position on your Careers page, for the <b>" +
+      formData.position +
+      "</b> position.  <br>See attachment for resume/cover letter.</p>" +
+      "Applicant Details:" +
+      "<ul><li><b>Name: </b> " +
+      formData.first +
+      " " +
+      formData.last +
+      "</li><li><b>Application Position: </b>" +
+      formData.position +
+      '</li><li><b>E-mail: </b><a href="mailto:' +
+      formData.email +
+      '?Subject=LITSCO%20Employment%20Inquiry%20Response" target="_top">' +
+      formData.email +
+      "</a></li><li><b>Phone: </b>" +
+      formData.phone +
+      "<li></li><b>Address: </b>" +
+      formData.address +
+      "</li><li><b>They are interested in this position because: </b>" +
+      formData.message +
+      "</li></ul>" +
+      "You can respond directly to this email, or create a new message clicking on the email address listed.",
+    attachments: [
+      {
+        filename: file.originalname,
+        content: file.buffer
+      }
+    ]
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    var output = {
+      flag: 1
     };
+    if (error) {
+      console.log("** ERROR: " + error);
+      output.flag = 0;
+      output.error = error;
+    } else {
+      console.log("** Message sent: " + info.response);
+      output.response = info.response;
+      output.success = true;
+    }
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        var output = {
-                flag: 1
-            };
-        if(error) {
-            console.log('** ERROR: ' + error);
-            output.flag = 0;
-            output.error = error;
-        } else {
-            console.log('** Message sent: ' + info.response);
-            output.response = info.response;
-            output.success = true;
-        }
-
-        res.json(output);
-    });
+    res.json(output);
+  });
+  transporter.close();
 });
 // Exports
 module.exports = apiRouter;
--- src/scripts/components/controller_contact.js
+++ src/scripts/components/controller_contact.js
@@ -1,82 +1,96 @@
-angular.module('app_litsco')
-	.controller('controller_contact', ['$scope', '$state', '$http', 'factory_meta', function ($scope, $state, $http, factory_meta) {
+angular.module("app_litsco").controller("controller_contact", [
+  "$scope",
+  "$state",
+  "$http",
+  "factory_meta",
+  function($scope, $state, $http, factory_meta) {
+    factory_meta.contact();
 
-		factory_meta.contact();
+    angular
+      .element(document.querySelector(".jumbo-div"))
+      .css("backgroundImage", 'url("/img/jumbo_contact.jpg")');
 
-		angular.element(document.querySelector('.jumbo-div')).css('backgroundImage', 'url("/img/jumbo_contact.jpg")');		
-			
-		$scope.originTrue = false;
-		$scope.formSubmitSuccess = false;
-		$scope.formSubmit = false;
-		$scope.litsco = '40.706940, -73.8613';
-		$scope.litscoAddress = 'LITSCO, 76-11 88th St, Glendale, NY 11385';
-		$scope.contactData = {
-			phone: '',
-			email: '',
-			name: '',
-			company: '',
-			message: ''
-		};
-		$scope.postData = {};
+    $scope.originTrue = false;
+    $scope.formSubmitSuccess = false;
+    $scope.formSubmit = false;
+    $scope.litsco = "40.706940, -73.8613";
+    $scope.litscoAddress = "LITSCO, 76-11 88th St, Glendale, NY 11385";
+    $scope.contactData = {
+      phone: "",
+      email: "",
+      name: "",
+      company: "",
+      message: ""
+    };
+    $scope.postData = {};
 
-		$scope.showDirections = function (origin) {
-			$('#directions-panel').addClass('directionsTrue');
-			$scope.originTrue = true;
-			$scope.originAddress = origin;
-		};
+    $scope.showDirections = function(origin) {
+      $("#directions-panel").addClass("directionsTrue");
+      $scope.originTrue = true;
+      $scope.originAddress = origin;
+    };
 
-		$scope.hideDirections = function () {
-			$('#directions-panel').removeClass('directionsTrue');
-			$scope.originTrue = false;
-			$scope.originAddress = '';
-		};
+    $scope.hideDirections = function() {
+      $("#directions-panel").removeClass("directionsTrue");
+      $scope.originTrue = false;
+      $scope.originAddress = "";
+    };
 
-		$scope.validateHuman = function(input, that) {
-            if (input == 4) {
-                angular.element(document.querySelector('#contact-submit-button')).removeAttr('disabled');
-            }
-        };
+    $scope.validateHuman = function(input, that) {
+      if (input == 4) {
+        angular
+          .element(document.querySelector("#contact-submit-button"))
+          .removeAttr("disabled");
+      }
+    };
 
-		$scope.postMail = function (contact) {
-			var contactFormHeight = $('.contact-form-wrapper').height();
-			$scope.formSubmit = true;
-			$('.contact-form-success').height(contactFormHeight);
-			// Check form validation
-			// if ($scope.contactForm.$invalid === true) {
-			// 	return;
-			// }
-			// wrap all your input values in $scope.postData
-			$scope.postData = angular.copy(contact);
+    $scope.postMail = function(contact) {
+      var contactFormHeight = $(".contact-form-wrapper").height();
+      $scope.formSubmit = true;
+      $(".contact-form-success").height(contactFormHeight);
+      // Check form validation
+      // if ($scope.contactForm.$invalid === true) {
+      // 	return;
+      // }
+      // wrap all your input values in $scope.postData
+      $scope.postData = angular.copy(contact);
 
-			var req = {
-				method: 'POST',
-				url: '/api/contact',
-				headers: {
-					'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-				},
-				data: $scope.postData
-			};
+      var req = {
+        method: "POST",
+        url: "/api/contact",
+        headers: {
+          "Access-Control-Allow-Headers":
+            "Origin, X-Requested-With, Content-Type, Accept"
+        },
+        data: $scope.postData
+      };
 
-			$http(req)
-				.then(function successCallback(response) {
-					$scope.contactData = {
-						phone: '',
-						email: '',
-						name: '',
-						company: '',
-						message: ''
-					};
-					$scope.contactForm.$setPristine();
-					$('.form-contact-input-field, .form-contact-submit').prop('disabled', true);
+      $http(req).then(
+        function successCallback(response) {
+          $scope.contactData = {
+            phone: "",
+            email: "",
+            name: "",
+            company: "",
+            message: ""
+          };
+          $scope.contactForm.$setPristine();
+          $(".form-contact-input-field, .form-contact-submit").prop(
+            "disabled",
+            true
+          );
 
-					$scope.formSubmitSuccess = true;
-					//do something after success
-					// this callback will be called asynchronously
-					// when the response is available
-				}, function errorCallback(response) {
-					//do something after error
-					// called asynchronously if an error occurs
-					// or server returns response with an error status.
-				});
-		};
-	}]);
+          $scope.formSubmitSuccess = true;
+          //do something after success
+          // this callback will be called asynchronously
+          // when the response is available
+        },
+        function errorCallback(response) {
+          //do something after error
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        }
+      );
+    };
+  }
+]);
