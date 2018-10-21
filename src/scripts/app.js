844d97446934a6ced500bf6c7b641ddb8575503b
Phil Lucks <philip.lucks@snapengage.com>
2018-10-21 08:57:54 -0600

    trusted text as html

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -55,4 +55,11 @@ angular
         optimized: false
       }
     });
-  });
+  })
+  .filter('trusted', ['$sce', function($sce) {
+    var div = document.createElement('div');
+    return function(text) {
+        div.innerHTML = text;
+        return $sce.trustAsHtml(div.textContent);
+    };
+}]);
